c67953701fb6a9a0614870a1d5505c69492d3afc
xiaobo <bo.xiao@ebonex.cc>
2023-02-20 18:05:03 +0800

    删除不用代码

--- src/main/java/com/panpan/maimiao/MaimiaoApplication.java
+++ src/main/java/com/panpan/maimiao/MaimiaoApplication.java
@@ -2,9 +2,6 @@ package com.panpan.maimiao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class MaimiaoApplication {
@@ -12,5 +9,4 @@ public class MaimiaoApplication {
     public static void main(String[] args) {
         SpringApplication.run(MaimiaoApplication.class, args);
     }
-
 }
