d454391df56dba70e3b2f5c8bd5636ea30b2bb2f
xiaobo <bo.xiao@ebonex.cc>
2023-02-22 08:27:32 +0800

    修改版本

--- maimiao-spring-boot-autoconfigure/src/main/java/com/panpan/maimiaoautoconfigure/config/MaimiaoAutoConfiguration.java
+++ maimiao-spring-boot-autoconfigure/src/main/java/com/panpan/maimiaoautoconfigure/config/MaimiaoAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.panpan.maimiaoautoconfigure.config;
 
 import com.panpan.maimiaoautoconfigure.service.ResponseAdvice;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 public class MaimiaoAutoConfiguration {
 
     @Bean
+    @ConditionalOnWebApplication
     public ResponseAdvice responseAdvice() {
         return new ResponseAdvice();
     }
